29429f03537dc066ff2bc5f69f2aa4763935bf09
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-30 13:32:33 +0600

    banner image animation using react motion

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,15 +1,28 @@
 import { motion as Motion } from "motion/react"
+import team1 from "../../assets/images/tim-van-der-kuip-CPs2X8JYmS8-unsplash.jpg"
+import team2 from "../../assets/images/vitaly-gariev-Kxo17w7BurY-unsplash.jpg"
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                />
-                <div>
-                    <Motion.h1 animate={{rotate:360, transition:{duration:5}}} className="text-4xl font-bold">The <Motion.span animate={{color: ['#ff5733', '#33ff33','#8a33ff'],
-                        transition:{duration:3, repeat:Infinity}
+                <div className="flex-1">
+                    <Motion.img
+                        src={team1}
+                        animate={{y:[20,150,20]}}
+                        transition={{duration:5, repeat:Infinity}}
+                        className="max-w-sm border-s-8 border-b-8 border-blue-600 rounded-br-4xl rounded-t-4xl shadow-2xl"
+                    />
+                    <Motion.img
+                        src={team2}
+                         animate={{x:[100,200,100]}}
+                        transition={{duration:10, delay:4, repeat:Infinity}}
+                        className="max-w-sm border-s-8 border-b-8 border-blue-600 rounded-br-4xl rounded-t-4xl shadow-2xl"
+                    />
+                </div>
+                <div className="flex-1">
+                    <Motion.h1 animate={{ rotate: 360, transition: { duration: 5 } }} className="text-4xl font-bold">The <Motion.span animate={{
+                        color: ['#ff5733', '#33ff33', '#8a33ff'],
+                        transition: { duration: 3, repeat: Infinity }
                     }}>Easiest Way</Motion.span> to get <br /> your New Job!</Motion.h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
